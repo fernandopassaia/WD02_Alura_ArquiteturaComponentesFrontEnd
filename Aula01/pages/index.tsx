d4342434bd8fb6ca5e4c870d9953cab414e7b2e2
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-12-21 17:00:16 +0000

    Aula 01 Criando meu primeiro arquivo de Thema e meu primeiro component Box

--- Aula01/pages/index.tsx
+++ Aula01/pages/index.tsx
@@ -1,5 +1,13 @@
+import Box from "../src/components/Box";
+import theme from "../src/theme/theme";
+
 export default function HomeScreen(){
 	return (
-		<div>Oi</div>
+		<Box
+			tag="main"
+			styleSheet={{
+				fontFamily: theme.typography.fontFamily
+			}}		
+		>Oi</Box>
 	)
 }
