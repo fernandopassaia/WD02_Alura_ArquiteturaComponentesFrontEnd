972510e1c425d8520ef0a2a508bf1262bf626863
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-12-23 21:49:44 +0000

    Variant de Texto arrumada

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,16 +1,30 @@
+import { BaseComponent } from "@src/theme/BaseComponent";
+import { StyleSheet } from "@src/theme/StyleSheet";
 import theme from "@src/theme/theme";
 import React from 'react';
-import Box from "../Box/Box";
 
 interface TextProps {
-  tag?: 'p' | 'li' | 'h1' | string;
+  variant?: 'display1';
+  tag?: 'p' | 'li' | 'h1' | 'h2' | 'h2' | string;
   children?: React.ReactNode;
+  styleSheet?: StyleSheet;
 }
-export default function Text(props: TextProps) {
+export default function Text({
+  styleSheet, 
+  variant,
+  ...props
+}: TextProps) {
+  //Eu irei receber a variante da página que chamar o Text (como a HomeScreen). A linha abaixo irá pegar
+  //essa variante dentro do arquivo typrography pelo array. Ira receber todas as propriedades e então
+  //propagar essa variante para o BaseComponent
+  const textVariant = theme.typography.variants[variant];
+
   return (
-    <Box
+    <BaseComponent
       styleSheet={{
         fontFamily: theme.typography.fontFamily,
+        ...textVariant,//repasso o textVariante para o component principal
+        ...styleSheet,
       }}
       {...props}
     />
@@ -19,4 +33,5 @@ export default function Text(props: TextProps) {
 
 Text.defaultProps = {
   tag: 'p',
+  variant: 'body2'//digo que meu DEfault é o Body2 (que é um tamanho médio razoável)
 };
