06da06de8ccbd9f0f9468ef63ffa47bd51def02d
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-12-23 14:56:57 +0000

    Final Aula 2.1

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 interface BoxProps {
   // Vamos resolver esse any depois
-  tag: any;
-  children: React.ReactNode;
-  styleSheet: StyleSheet;
+  tag?: any;
+  children?: React.ReactNode;
+  styleSheet?: StyleSheet;
 }
 export default function Box({ styleSheet, children, tag }: BoxProps) {
   const Tag = tag || 'div';
