88b609dd34f7a94afb73048e620541a251881900
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-12-29 22:18:27 +0000

    Bulding LocalStorage (32m)

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,4 @@
 import Box from "@src/components/Box/Box";
-import Link from "@src/components/Link/Link";
-import Text from "@src/components/Text/Text";
 import { useTheme } from "@src/theme/ThemeProvider";
 import Background from "./patterns/Background/Background";
 import Feed from "./patterns/Feed/Feed";
